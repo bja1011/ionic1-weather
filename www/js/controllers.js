7021cc285b676ab2275e7288de21bf1d4865315f
adam kaczmarek <kaczmarek@oskarwegner.pl>
2016-12-04 22:48:25 +0100

    get pictures for current location

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,56 +1,66 @@
 angular.module('app.controllers', [])
 
-.controller('weatherCtrl', ['$scope', '$stateParams','weatherService','miscService',
+  .controller('weatherCtrl', ['$scope', '$stateParams', 'weatherService', 'miscService',
 
 
-function ($scope, $stateParams,weatherService,miscService) {
+    function ($scope, $stateParams, weatherService, miscService) {
 
-  var t = $scope;
-  t.weatherData = {};
+      var t = $scope;
+      t.weatherData = {};
 
-  miscService.getLocation()
-    .then(function (location) {
-      console.log(location)
-      console.log(location.coords.latitude,location.coords.longitude);
+      t.getWeather = function () {
+        miscService.getLocation()
+          .then(function (location) {
+            miscService.getPictures(location.coords.latitude, location.coords.longitude)
+              .then(function (resp) {
+                t.pictures = resp.data;
+                t.mainImage = t.flickrItemImage(t.pictures.photos.photo[0]);
+              })
+            weatherService.get(location)
+              .then(function (resp) {
+                t.weatherData = resp.data;
+              })
 
-      weatherService.get(location)
-        .then(function (resp) {
-          t.weatherData = resp.data;
-        })
+          })
+      }
 
-    })
+      t.getWeather();
 
-
-}])
-
-.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-
-
-function ($scope, $stateParams) {
-
-
-}])
-
-.controller('photosCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+      t.flickrItemImage = function (item) {
+        return "http://farm" + item.farm + ".static.flickr.com/" + item.server + "/" + item.id + "_" + item.secret + ".jpg";
+      }
 
 
-function ($scope, $stateParams) {
+    }])
+
+  .controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 
 
-}])
-
-.controller('settingsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+    function ($scope, $stateParams) {
 
 
-function ($scope, $stateParams) {
+    }])
+
+  .controller('photosCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 
 
-}])
-
-.controller('addLocationCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+    function ($scope, $stateParams) {
 
 
-function ($scope, $stateParams) {
+    }])
+
+  .controller('settingsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 
 
-}])
+    function ($scope, $stateParams) {
+
+
+    }])
+
+  .controller('addLocationCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+
+
+    function ($scope, $stateParams) {
+
+
+    }])
--- www/js/services.js
+++ www/js/services.js
@@ -1,36 +1,50 @@
 angular.module('app.services', [])
 
-.service('weatherService', function($http){
-  var service = {};
+  .service('weatherService', function ($http) {
+    var service = {};
 
-  service.get = function(location) {
-    if(typeof location == 'string') {
-      return $http.get('http://api.openweathermap.org/data/2.5/forecast?q='+location+'&appid=1929fffb238baf259922bfbb99ae5a73');
-    } else return $http.get('http://api.openweathermap.org/data/2.5/forecast?lat='+location.coords.latitude+'&lon='+location.coords.longitude+'&appid=1929fffb238baf259922bfbb99ae5a73');
-  };
+    service.get = function (location) {
+      if (typeof location == 'string') {
+        return $http.get('http://api.openweathermap.org/data/2.5/forecast?q=' + location + '&appid=1929fffb238baf259922bfbb99ae5a73');
+      } else return $http.get('http://api.openweathermap.org/data/2.5/forecast?lat=' + location.coords.latitude + '&lon=' + location.coords.longitude + '&appid=1929fffb238baf259922bfbb99ae5a73');
+    };
 
-  return service;
-})
+    return service;
+  })
 
-.service('miscService',function ($q) {
-  var service = {};
+  .service('miscService', function ($q,$http) {
+    var service = {};
 
-  service.getLocation = function() {
+    service.getLocation = function () {
 
-    return $q(function(resolve, reject) {
+      return $q(function (resolve, reject) {
 
-      function onError(error) {
-        reject(error);
-      }
-      var options = { enableHighAccuracy: true };
-      navigator.geolocation.getCurrentPosition(function (position) {
-        resolve(position);
-      }, onError,options);
+        function onError(error) {
+          reject(error);
+        }
 
-    });
+        function onSuccess(position) {
+          resolve(position);
+        }
 
-  };
+        var options = {enableHighAccuracy: true};
 
-  return service;
-});
+        navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+
+      });
+
+    };
+
+    service.getPictures = function(latitude, longitude) {
+
+      var queryString =
+        "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=1543c174a55e5b9b83d44ce2945005a5&lat="
+        + latitude + "&lon=" + longitude + "&format=json&nojsoncallback=1&tags=sunrise";
+
+      return $http.get(queryString);
+
+    };
+
+    return service;
+  });
 
